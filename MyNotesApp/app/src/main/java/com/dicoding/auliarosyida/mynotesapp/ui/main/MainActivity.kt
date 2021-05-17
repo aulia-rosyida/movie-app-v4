944a8be5932052f7f5ca445b04091cec4c010059
aulia-rosyida <rosyidaaulia24@gmail.com>
2021-05-17 16:25:05 +0700

    connect view model with main activity

--- MyNotesApp/app/src/main/java/com/dicoding/auliarosyida/mynotesapp/ui/main/MainActivity.kt
+++ MyNotesApp/app/src/main/java/com/dicoding/auliarosyida/mynotesapp/ui/main/MainActivity.kt
@@ -2,9 +2,13 @@ package com.dicoding.auliarosyida.mynotesapp.ui.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.dicoding.auliarosyida.mynotesapp.R
 import com.dicoding.auliarosyida.mynotesapp.databinding.ActivityMainBinding
+import com.dicoding.auliarosyida.mynotesapp.db.Note
+import com.dicoding.auliarosyida.mynotesapp.helper.ViewModelFactory
 
 class MainActivity : AppCompatActivity() {
 
@@ -17,6 +21,9 @@ class MainActivity : AppCompatActivity() {
         _activityMainBinding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding?.root)
 
+        val mainViewModel = obtainViewModel(this@MainActivity)
+        mainViewModel.getAllNotes().observe(this, noteObserver)
+
         adapter = NoteAdapter(this@MainActivity)
         binding?.rvNotes?.layoutManager = LinearLayoutManager(this)
         binding?.rvNotes?.setHasFixedSize(true)
@@ -26,5 +33,16 @@ class MainActivity : AppCompatActivity() {
     override fun onDestroy() {
         super.onDestroy()
         _activityMainBinding = null
+    }
+
+    private fun obtainViewModel(activity: AppCompatActivity): MainViewModel {
+        val factory = ViewModelFactory.getInstance(activity.application)
+        return ViewModelProvider(activity, factory).get(MainViewModel::class.java)
+    }
+
+    private val noteObserver = Observer<List<Note>> { noteList ->
+        if (noteList != null) {
+            adapter.setListNotes(noteList)
+        }
     }
 }
