1b5858d467fa3a5551e04bc3f5136aa26b4791b9
aulia-rosyida <rosyidaaulia24@gmail.com>
2021-05-17 16:10:39 +0700

    implement viewModelFactory to add context when calling viewModel in activity

--- MyNotesApp/app/src/main/java/com/dicoding/auliarosyida/mynotesapp/helper/ViewModelFactory.kt
+++ MyNotesApp/app/src/main/java/com/dicoding/auliarosyida/mynotesapp/helper/ViewModelFactory.kt
@@ -1,4 +1,35 @@
 package com.dicoding.auliarosyida.mynotesapp.helper
 
-class ViewModelFactory {
+import android.app.Application
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.dicoding.auliarosyida.mynotesapp.ui.insert.NoteAddUpdateViewModel
+import com.dicoding.auliarosyida.mynotesapp.ui.main.MainViewModel
+
+/**
+ * Kelas ini berfungsi untuk menambahkan context ketika memanggil kelas ViewModel di dalam Activity
+ * */
+class ViewModelFactory private constructor(private val mApplication: Application) : ViewModelProvider.NewInstanceFactory() {
+    companion object {
+        @Volatile
+        private var INSTANCE: ViewModelFactory? = null
+        @JvmStatic
+        fun getInstance(application: Application): ViewModelFactory {
+            if (INSTANCE == null) {
+                synchronized(ViewModelFactory::class.java) {
+                    INSTANCE = ViewModelFactory(application)
+                }
+            }
+            return INSTANCE as ViewModelFactory
+        }
+    }
+
+    override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(MainViewModel::class.java)) {
+            return MainViewModel(mApplication) as T
+        } else if (modelClass.isAssignableFrom(NoteAddUpdateViewModel::class.java)) {
+            return NoteAddUpdateViewModel(mApplication) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class: ${modelClass.name}")
+    }
 }
