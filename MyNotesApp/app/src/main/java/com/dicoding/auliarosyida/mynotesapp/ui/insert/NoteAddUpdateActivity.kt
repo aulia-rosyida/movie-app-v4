dae962b2f762bd39cf53bbae11e25be2625b114d
aulia-rosyida <rosyidaaulia24@gmail.com>
2021-05-17 16:20:18 +0700

    delete item from db

--- MyNotesApp/app/src/main/java/com/dicoding/auliarosyida/mynotesapp/ui/insert/NoteAddUpdateActivity.kt
+++ MyNotesApp/app/src/main/java/com/dicoding/auliarosyida/mynotesapp/ui/insert/NoteAddUpdateActivity.kt
@@ -3,6 +3,9 @@ package com.dicoding.auliarosyida.mynotesapp.ui.insert
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import androidx.appcompat.app.AlertDialog
 import androidx.lifecycle.ViewModelProvider
 import com.dicoding.auliarosyida.mynotesapp.R
 import com.dicoding.auliarosyida.mynotesapp.databinding.ActivityNoteAddUpdateBinding
@@ -105,6 +108,25 @@ class NoteAddUpdateActivity : AppCompatActivity() {
         binding?.btnSubmit?.text = btnTitle
     }
 
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        if (isEdit) {
+            menuInflater.inflate(R.menu.menu_form, menu)
+        }
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.action_delete -> showAlertDialog(ALERT_DIALOG_DELETE)
+            android.R.id.home -> showAlertDialog(ALERT_DIALOG_CLOSE)
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    override fun onBackPressed() {
+        showAlertDialog(ALERT_DIALOG_CLOSE)
+    }
+
     override fun onDestroy() {
         super.onDestroy()
         _activityNoteAddUpdateBinding = null
@@ -113,5 +135,38 @@ class NoteAddUpdateActivity : AppCompatActivity() {
     private fun obtainViewModel(activity: AppCompatActivity): NoteAddUpdateViewModel {
         val factory = ViewModelFactory.getInstance(activity.application)
         return ViewModelProvider(activity, factory).get(NoteAddUpdateViewModel::class.java)
+    }
+
+    private fun showAlertDialog(type: Int) {
+        val isDialogClose = type == ALERT_DIALOG_CLOSE
+        val dialogTitle: String
+        val dialogMessage: String
+        if (isDialogClose) {
+            dialogTitle = getString(R.string.cancel)
+            dialogMessage = getString(R.string.message_cancel)
+        } else {
+            dialogMessage = getString(R.string.message_delete)
+            dialogTitle = getString(R.string.delete)
+        }
+
+        val alertDialogBuilder = AlertDialog.Builder(this)
+        with(alertDialogBuilder) {
+            setTitle(dialogTitle)
+            setMessage(dialogMessage)
+            setCancelable(false)
+            setPositiveButton(getString(R.string.yes)) { _, _ ->
+                if (!isDialogClose) {
+                    noteAddUpdateViewModel.delete(note as Note)
+                    val intent = Intent()
+                    intent.putExtra(EXTRA_POSITION, position)
+                    setResult(RESULT_DELETE, intent)
+                }
+                finish()
+            }
+            setNegativeButton(getString(R.string.no)) { dialog, _ -> dialog.cancel() }
+        }
+
+        val alertDialog = alertDialogBuilder.create()
+        alertDialog.show()
     }
 }
