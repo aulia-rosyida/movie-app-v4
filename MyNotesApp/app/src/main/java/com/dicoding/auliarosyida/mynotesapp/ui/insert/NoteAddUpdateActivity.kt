f8e2d6931e37b23149c5fbc8c4a518bf6e5f6159
aulia-rosyida <rosyidaaulia24@gmail.com>
2021-05-17 16:15:08 +0700

    init view in layout

--- MyNotesApp/app/src/main/java/com/dicoding/auliarosyida/mynotesapp/ui/insert/NoteAddUpdateActivity.kt
+++ MyNotesApp/app/src/main/java/com/dicoding/auliarosyida/mynotesapp/ui/insert/NoteAddUpdateActivity.kt
@@ -3,8 +3,26 @@ package com.dicoding.auliarosyida.mynotesapp.ui.insert
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.dicoding.auliarosyida.mynotesapp.R
+import com.dicoding.auliarosyida.mynotesapp.db.Note
 
 class NoteAddUpdateActivity : AppCompatActivity() {
+
+    companion object {
+        const val EXTRA_NOTE = "extra_note"
+        const val EXTRA_POSITION = "extra_position"
+        const val REQUEST_ADD = 100
+        const val RESULT_ADD = 101
+        const val REQUEST_UPDATE = 200
+        const val RESULT_UPDATE = 201
+        const val RESULT_DELETE = 301
+        const val ALERT_DIALOG_CLOSE = 10
+        const val ALERT_DIALOG_DELETE = 20
+    }
+
+    private var isEdit = false
+    private var note: Note? = null
+    private var position = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_note_add_update)
