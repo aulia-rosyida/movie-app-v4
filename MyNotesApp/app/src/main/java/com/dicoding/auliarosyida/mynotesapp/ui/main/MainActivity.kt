742908c877cd8b469e5c2db0502655adc2ca421f
aulia-rosyida <rosyidaaulia24@gmail.com>
2021-05-17 16:24:08 +0700

    init RecyclerView, Floating action button, NoteAdapter in main Activity

--- MyNotesApp/app/src/main/java/com/dicoding/auliarosyida/mynotesapp/ui/main/MainActivity.kt
+++ MyNotesApp/app/src/main/java/com/dicoding/auliarosyida/mynotesapp/ui/main/MainActivity.kt
@@ -2,11 +2,29 @@ package com.dicoding.auliarosyida.mynotesapp.ui.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.dicoding.auliarosyida.mynotesapp.R
+import com.dicoding.auliarosyida.mynotesapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private var _activityMainBinding: ActivityMainBinding? = null
+    private val binding get() = _activityMainBinding
+    private lateinit var adapter: NoteAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        _activityMainBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
+
+        adapter = NoteAdapter(this@MainActivity)
+        binding?.rvNotes?.layoutManager = LinearLayoutManager(this)
+        binding?.rvNotes?.setHasFixedSize(true)
+        binding?.rvNotes?.adapter = adapter
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _activityMainBinding = null
     }
 }
