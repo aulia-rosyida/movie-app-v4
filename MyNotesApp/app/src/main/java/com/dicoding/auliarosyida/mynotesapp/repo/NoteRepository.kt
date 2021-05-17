b816929b14581351263e135fd21308e0e50814e2
aulia-rosyida <rosyidaaulia24@gmail.com>
2021-05-17 15:22:40 +0700

    repository class to connect viewModel with db or resource data

---
+++ MyNotesApp/app/src/main/java/com/dicoding/auliarosyida/mynotesapp/repo/NoteRepository.kt
@@ -0,0 +1,37 @@
+package com.dicoding.auliarosyida.mynotesapp.repo
+
+import android.app.Application
+import androidx.lifecycle.LiveData
+import com.dicoding.auliarosyida.mynotesapp.db.Note
+import com.dicoding.auliarosyida.mynotesapp.db.NoteDao
+import com.dicoding.auliarosyida.mynotesapp.db.NoteRoomDatabase
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
+
+/**
+ * Kelas ini berfungsi sebagai penghubung antara ViewModel dengan database atau resource data.
+ * */
+class NoteRepository (application: Application) {
+
+    private val mNotesDao: NoteDao
+    private val executorService: ExecutorService = Executors.newSingleThreadExecutor()
+
+    init {
+        val db = NoteRoomDatabase.getDatabase(application)
+        mNotesDao = db.noteDao()
+    }
+
+    fun getAllNotes(): LiveData<List<Note>> = mNotesDao.getAllNotes()
+
+    fun insert(note: Note) {
+        executorService.execute { mNotesDao.insert(note) }
+    }
+
+    fun delete(note: Note) {
+        executorService.execute { mNotesDao.delete(note) }
+    }
+
+    fun update(note: Note) {
+        executorService.execute { mNotesDao.update(note) }
+    }
+}
