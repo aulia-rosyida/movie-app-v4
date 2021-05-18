093b6183f268ae31a1ab0badc95c4bcf3bb00bfd
aulia-rosyida <rosyidaaulia24@gmail.com>
2021-05-18 13:59:41 +0700

    new class to give status success/failed in NetworkAPI simulation

---
+++ Academy/app/src/main/java/com/dicoding/auliarosyida/academy/data/source/remote/StatusResponse.kt
@@ -0,0 +1,10 @@
+package com.dicoding.auliarosyida.academy.data.source.remote
+
+/**
+ * kelas baru untuk memberitahukan simulasi NetworkAPI berhasil atau tidak.
+ * */
+enum class StatusResponse {
+    SUCCESS,
+    EMPTY,
+    ERROR
+}
