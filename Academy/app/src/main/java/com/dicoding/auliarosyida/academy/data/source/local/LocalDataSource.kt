89383f0b150a31f66c63d8d982e7d490c780808b
aulia-rosyida <rosyidaaulia24@gmail.com>
2021-05-18 13:55:20 +0700

    LocalDataSource utk kelola db local

---
+++ Academy/app/src/main/java/com/dicoding/auliarosyida/academy/data/source/local/LocalDataSource.kt
@@ -0,0 +1,51 @@
+package com.dicoding.auliarosyida.academy.data.source.local
+
+import androidx.lifecycle.LiveData
+import com.dicoding.auliarosyida.academy.data.CourseEntity
+import com.dicoding.auliarosyida.academy.data.CourseWithModule
+import com.dicoding.auliarosyida.academy.data.ModuleEntity
+import com.dicoding.auliarosyida.academy.data.source.local.room.AcademyDao
+
+/**
+ * kelas LocalDataSource dan hubungkan AcademyDao ke LocalDataSource.
+ * LocalDataSource bisa Anda pakai untuk mengelola database local.
+ * */
+class LocalDataSource private constructor(private val mAcademyDao: AcademyDao) {
+    companion object {
+        private var INSTANCE: LocalDataSource? = null
+
+        fun getInstance(academyDao: AcademyDao): LocalDataSource =
+            INSTANCE ?: LocalDataSource(academyDao)
+    }
+
+    fun getAllCourses(): LiveData<List<CourseEntity>> = mAcademyDao.getCourses()
+
+    fun getBookmarkedCourses(): LiveData<List<CourseEntity>> = mAcademyDao.getBookmarkedCourse()
+
+    fun getCourseWithModules(courseId: String): LiveData<CourseWithModule> =
+        mAcademyDao.getCourseWithModuleById(courseId)
+
+    fun getAllModulesByCourse(courseId: String): LiveData<List<ModuleEntity>> =
+        mAcademyDao.getModulesByCourseId(courseId)
+
+    fun insertCourses(courses: List<CourseEntity>) = mAcademyDao.insertCourses(courses)
+
+    fun insertModules(modules: List<ModuleEntity>) = mAcademyDao.insertModules(modules)
+
+    fun setCourseBookmark(course: CourseEntity, newState: Boolean) {
+        course.bookmarked = newState
+        mAcademyDao.updateCourse(course)
+    }
+
+    fun getModuleWithContent(moduleId: String): LiveData<ModuleEntity> =
+        mAcademyDao.getModuleById(moduleId)
+
+    fun updateContent(content: String, moduleId: String) {
+        mAcademyDao.updateModuleByContent(content, moduleId)
+    }
+
+    fun setReadModule(module: ModuleEntity) {
+        module.read = true
+        mAcademyDao.updateModule(module)
+    }
+}
