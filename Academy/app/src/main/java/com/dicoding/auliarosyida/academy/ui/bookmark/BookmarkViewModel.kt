f9143185622929cdfe5c5458f129f29d775126ba
aulia-rosyida <rosyidaaulia24@gmail.com>
2021-05-18 22:28:13 +0700

    change code in viewModel to PagedList

--- Academy/app/src/main/java/com/dicoding/auliarosyida/academy/ui/bookmark/BookmarkViewModel.kt
+++ Academy/app/src/main/java/com/dicoding/auliarosyida/academy/ui/bookmark/BookmarkViewModel.kt
@@ -2,10 +2,11 @@ package com.dicoding.auliarosyida.academy.ui.bookmark
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
+import androidx.paging.PagedList
 import com.dicoding.auliarosyida.academy.data.source.local.entity.CourseEntity
 import com.dicoding.auliarosyida.academy.data.AcademyRepository
 
 class BookmarkViewModel(private val academyRepository: AcademyRepository) : ViewModel() {
 
-    fun getBookmarks(): LiveData<List<CourseEntity>> = academyRepository.getBookmarkedCourses()
+    fun getBookmarks(): LiveData<PagedList<CourseEntity>> = academyRepository.getBookmarkedCourses()
 }
