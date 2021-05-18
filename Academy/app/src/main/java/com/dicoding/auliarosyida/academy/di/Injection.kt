85bd22c842f5e879d3002b6ed8ed67a572c056be
aulia-rosyida <rosyidaaulia24@gmail.com>
2021-05-18 15:53:34 +0700

    change injection because change constructor before

--- Academy/app/src/main/java/com/dicoding/auliarosyida/academy/di/Injection.kt
+++ Academy/app/src/main/java/com/dicoding/auliarosyida/academy/di/Injection.kt
@@ -2,7 +2,10 @@ package com.dicoding.auliarosyida.academy.di
 
 import android.content.Context
 import com.dicoding.auliarosyida.academy.data.source.AcademyRepository
+import com.dicoding.auliarosyida.academy.data.source.local.LocalDataSource
+import com.dicoding.auliarosyida.academy.data.source.local.room.AcademyDatabase
 import com.dicoding.auliarosyida.academy.data.source.remote.RemoteDataSource
+import com.dicoding.auliarosyida.academy.utils.AppExecutors
 import com.dicoding.auliarosyida.academy.utils.JsonHelper
 
 /**
@@ -13,8 +16,12 @@ object Injection {
 
     fun provideRepository(context: Context): AcademyRepository {
 
-        val remoteDataSource = RemoteDataSource.getInstance(JsonHelper(context))
+        val database = AcademyDatabase.getInstance(context)
 
-        return AcademyRepository.getInstance(remoteDataSource)
+        val remoteDataSource = RemoteDataSource.getInstance(JsonHelper(context))
+        val localDataSource = LocalDataSource.getInstance(database.academyDao())
+        val appExecutors = AppExecutors()
+
+        return AcademyRepository.getInstance(remoteDataSource,localDataSource, appExecutors)
     }
 }
