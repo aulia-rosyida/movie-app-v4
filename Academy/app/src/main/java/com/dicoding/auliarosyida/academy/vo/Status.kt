21a4ccd05c39cf3614e5163f32a78e9d5d1d9b06
aulia-rosyida <rosyidaaulia24@gmail.com>
2021-05-18 14:37:09 +0700

    implement value object

---
+++ Academy/app/src/main/java/com/dicoding/auliarosyida/academy/vo/Status.kt
@@ -0,0 +1,10 @@
+package com.dicoding.auliarosyida.academy.vo
+
+/**
+ * Kelas Status digunakan sebagai indikator dari sukses, gagal dan loading
+ * */
+enum class Status {
+    SUCCESS,
+    ERROR,
+    LOADING
+}
