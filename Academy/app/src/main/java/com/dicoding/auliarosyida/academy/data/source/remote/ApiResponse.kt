4ffb354c1c93496754d0a46f5670e8cf6d4114b6
aulia-rosyida <rosyidaaulia24@gmail.com>
2021-05-18 14:01:41 +0700

    ApiResonse utk bungkus info dari remoteDataSource

--- Academy/app/src/main/java/com/dicoding/auliarosyida/academy/data/source/remote/ApiResponse.kt
+++ Academy/app/src/main/java/com/dicoding/auliarosyida/academy/data/source/remote/ApiResponse.kt
@@ -1,5 +1,8 @@
 package com.dicoding.auliarosyida.academy.data.source.remote
 
+/**
+ * ApiResponse nantinya digunakan untuk membungkus informasi dari RemoteDataSource
+ * */
 class ApiResponse<T>(val status: StatusResponse, val body: T, val message: String?) {
     companion object {
         fun <T> success(body: T): ApiResponse<T> = ApiResponse(StatusResponse.SUCCESS, body, null)
