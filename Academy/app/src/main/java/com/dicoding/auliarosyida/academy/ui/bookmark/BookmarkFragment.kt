84b15b435677bbf297c748d08dbfb7dd7e7fd042
aulia-rosyida <rosyidaaulia24@gmail.com>
2021-05-19 09:32:42 +0700

    layout untuk sw600dp (done)

--- Academy/app/src/main/java/com/dicoding/auliarosyida/academy/ui/bookmark/BookmarkFragment.kt
+++ Academy/app/src/main/java/com/dicoding/auliarosyida/academy/ui/bookmark/BookmarkFragment.kt
@@ -80,7 +80,7 @@ class BookmarkFragment : Fragment(), BookmarkFragmentCallback {
                 courseEntity?.let { viewModel.setBookmark(it) }
 
                 val snackbar = Snackbar.make(view as View, R.string.message_undo, Snackbar.LENGTH_LONG)
-                snackbar.setAction(R.string.message_ok) { v ->
+                snackbar.setAction(R.string.message_ok) { _ ->
                     courseEntity?.let { viewModel.setBookmark(it) }
                 }
                 snackbar.show()
