45be1b686b26ece5c6fbe9904c55eb0478f075d2
aulia-rosyida <rosyidaaulia24@gmail.com>
2021-05-19 18:30:20 +0700

    make tvshow entity

---
+++ MoviesApp/app/src/main/java/com/dicoding/auliarosyida/moviesapp/model/source/localsource/entity/TvShowEntity.kt
@@ -0,0 +1,45 @@
+package com.dicoding.auliarosyida.moviesapp.model.source.localsource.entity
+
+import androidx.annotation.NonNull
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "tvshowentities")
+data class TvShowEntity (
+
+    @PrimaryKey
+    @NonNull
+    @ColumnInfo(name = "tvShowId")
+    var tvShowId: String,
+
+    @ColumnInfo(name = "poster")
+    var poster: String,
+
+    @ColumnInfo(name = "title")
+    var title: String,
+
+    @ColumnInfo(name = "quote")
+    var quote: String,
+
+    @ColumnInfo(name = "overview")
+    var overview: String,
+
+    @ColumnInfo(name = "releaseYear")
+    var releaseYear: String,
+
+    @ColumnInfo(name = "genre")
+    var genre: String,
+
+    @ColumnInfo(name = "duration")
+    var duration: String,
+
+    @ColumnInfo(name = "status")
+    var status: String,
+
+    @ColumnInfo(name = "originalLanguage")
+    var originalLanguage: String,
+
+    @ColumnInfo(name = "bookmarked")
+    var bookmarked: Boolean = false
+)
