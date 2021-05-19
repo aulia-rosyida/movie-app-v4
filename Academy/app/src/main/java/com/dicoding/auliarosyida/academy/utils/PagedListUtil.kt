cb408e50848ced2d2d6f553979bc19b1229e90d0
aulia-rosyida <rosyidaaulia24@gmail.com>
2021-05-19 09:52:39 +0700

    new class as helper PagedList

---
+++ Academy/app/src/main/java/com/dicoding/auliarosyida/academy/utils/PagedListUtil.kt
@@ -0,0 +1,18 @@
+package com.dicoding.auliarosyida.academy.utils
+
+import androidx.paging.PagedList
+
+/** kelas baru yang berfungsi sebagai kelas pembantu PagedList. */
+object PagedListUtil {
+
+    fun <T> mockPagedList(list: List<T>): PagedList<T> {
+        val pagedList = mock(PagedList::class.java) as PagedList<T>
+        `when`(pagedList[anyInt()]).then { invocation ->
+            val index = invocation.arguments.first() as Int
+            list[index]
+        }
+        `when`(pagedList.size).thenReturn(list.size)
+
+        return pagedList
+    }
+}
