c385cdc4e9c5525ceee885356cb7b0548f20bc88
aulia-rosyida <rosyidaaulia24@gmail.com>
2021-05-19 19:53:59 +0700

    added ResourceWrapData in interface MovieDataSource and add 2 new method to set favorite

--- MoviesApp/app/src/main/java/com/dicoding/auliarosyida/moviesapp/model/source/InterfaceMovieDataSource.kt
+++ MoviesApp/app/src/main/java/com/dicoding/auliarosyida/moviesapp/model/source/InterfaceMovieDataSource.kt
@@ -1,14 +1,21 @@
 package com.dicoding.auliarosyida.moviesapp.model.source
 
 import androidx.lifecycle.LiveData
+import com.dicoding.auliarosyida.moviesapp.model.source.localsource.entity.MovieEntity
+import com.dicoding.auliarosyida.moviesapp.model.source.localsource.entity.TvShowEntity
 import com.dicoding.auliarosyida.moviesapp.model.source.remotesource.response.MovieResponse
+import com.dicoding.auliarosyida.moviesapp.valueobject.ResourceWrapData
 
 interface InterfaceMovieDataSource {
-    fun getAllMovies(): LiveData<List<MovieResponse>>
+    fun getAllMovies(): LiveData<ResourceWrapData<List<MovieResponse>>>
 
-    fun getAllTvShows(): LiveData<List<MovieResponse>>
+    fun getAllTvShows(): LiveData<ResourceWrapData<List<MovieResponse>>>
 
-    fun getDetailMovie(movieId: String): LiveData<MovieResponse>
+    fun getDetailMovie(movieId: String): LiveData<ResourceWrapData<MovieResponse>>
 
-    fun getDetailTvShow(tvShowId: String): LiveData<MovieResponse>
+    fun getDetailTvShow(tvShowId: String): LiveData<ResourceWrapData<MovieResponse>>
+
+    fun setFavoriteMovie(movie: MovieEntity, state: Boolean)
+
+    fun setFavoriteTvShow(movie: TvShowEntity, state: Boolean)
 }
