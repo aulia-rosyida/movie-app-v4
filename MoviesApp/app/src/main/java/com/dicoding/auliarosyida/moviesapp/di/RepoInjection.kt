444645d4c3bbf464b0980a1d9855d39e31974c22
aulia-rosyida <rosyidaaulia24@gmail.com>
2021-05-20 00:20:57 +0700

    memperbarui constructor di Injection

--- MoviesApp/app/src/main/java/com/dicoding/auliarosyida/moviesapp/di/RepoInjection.kt
+++ MoviesApp/app/src/main/java/com/dicoding/auliarosyida/moviesapp/di/RepoInjection.kt
@@ -2,7 +2,10 @@ package com.dicoding.auliarosyida.moviesapp.di
 
 import android.content.Context
 import com.dicoding.auliarosyida.moviesapp.model.source.MovieRepository
+import com.dicoding.auliarosyida.moviesapp.model.source.localsource.LocalMovieDataSource
+import com.dicoding.auliarosyida.moviesapp.model.source.localsource.room.MovieBuilderDatabase
 import com.dicoding.auliarosyida.moviesapp.model.source.remotesource.RemoteMovieDataSource
+import com.dicoding.auliarosyida.moviesapp.utils.AppThreadExecutors
 import com.dicoding.auliarosyida.moviesapp.utils.JsonResponseHelper
 
 /**
@@ -11,8 +14,12 @@ import com.dicoding.auliarosyida.moviesapp.utils.JsonResponseHelper
 object RepoInjection {
     fun provideMovieRepository(context: Context): MovieRepository {
 
-        val remoteMovieDataSource = RemoteMovieDataSource.getInstance(JsonResponseHelper(context))
+        val db = MovieBuilderDatabase.getInstance(context)
 
-        return MovieRepository.getInstance(remoteMovieDataSource)
+        val remoteMovieDataSource = RemoteMovieDataSource.getInstance(JsonResponseHelper(context))
+        val localMovieDataSource = LocalMovieDataSource.getInstance(db.movieDao())
+        val appThreadExecutors = AppThreadExecutors()
+
+        return MovieRepository.getInstance(remoteMovieDataSource, localMovieDataSource, appThreadExecutors)
     }
 }
