32abd4219e9cc7a63d6820cb53268b5f13e09862
aulia-rosyida <rosyidaaulia24@gmail.com>
2021-06-18 15:15:44 +0700

    navigate to modul fav

--- MoviesApp/app/src/main/java/com/dicoding/auliarosyida/moviesapp/landingpage/LandingActivity.kt
+++ MoviesApp/app/src/main/java/com/dicoding/auliarosyida/moviesapp/landingpage/LandingActivity.kt
@@ -1,7 +1,9 @@
 package com.dicoding.auliarosyida.moviesapp.landingpage
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.dicoding.auliarosyida.moviesapp.databinding.ActivityLandingBinding
 
 class LandingActivity : AppCompatActivity() {
@@ -15,5 +17,17 @@ class LandingActivity : AppCompatActivity() {
         activityLandingBinding.tabs.setupWithViewPager(activityLandingBinding.viewPager)
 
         supportActionBar?.elevation = 0f
+
+        activityLandingBinding.fab.setOnClickListener {
+            try {
+                moveToChatActivity()
+            } catch (e: Exception){
+                Toast.makeText(this, "Module not found", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    private fun moveToChatActivity() {
+        startActivity(Intent(this, Class.forName("com.dicoding.auliarosyida.moviesapp.fav.FavActivity")))
     }
 }
